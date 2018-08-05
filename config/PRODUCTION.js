d0615a26479069fc4ede442bc097625f585754f1
Lior Rabin <lior@colu.com>
2018-08-05 14:51:55 +0300

    latest osseus (with osseus-router support for jwt)

--- config/PRODUCTION.js
+++ config/PRODUCTION.js
@@ -3,7 +3,7 @@ module.exports = {
   OSSEUS_LOGGER_LOG_LEVEL: 'debug',
   OSSEUS_SERVER_DEPENDENCIES: ['logger', 'mongo'],
   OSSEUS_SERVER_PORT: '8080',
-  OSSEUS_SERVER_MORGAN_FORMAT: ':date[iso] method=":method", url=":url", statusCode=":status", route=":route", host=":host", client-ip=":client-ip", user-agent=":user-agent", request-id=":request-id", httpVersion=":http-version", responseTime=":response-time"',
+  OSSEUS_SERVER_MORGAN_FORMAT: ':date[iso] method=":method", url=":url", statusCode=":status", route=":route", host=":host", client-ip=":client-ip", user-agent=":user-agent", httpVersion=":http-version", responseTime=":response-time"',
   OSSEUS_ROUTER_DEPENDENCIES: ['logger', 'server'],
   OSSEUS_ROUTER_ROUTES_PATH: '/app/routes',
   OSSEUS_ROUTER_CONTROLLERS_PATH: '/app/controllers',
