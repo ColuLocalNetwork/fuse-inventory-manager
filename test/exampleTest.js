90f83ce758d71d33706db8a17e1500ca81129062
Lior Rabin <lior@colu.com>
2018-08-16 12:51:24 +0300

    lint

--- test/exampleTest.js
+++ test/exampleTest.js
@@ -1,12 +1,11 @@
 contract('ColuLocalNetwork', async (accounts) => {
-  const ColuLocalNetwork = artifacts.require("cln-solidity/contracts/ColuLocalNetwork.sol");
-  const totalSupply = 1000;
+  const ColuLocalNetwork = artifacts.require('cln-solidity/contracts/ColuLocalNetwork.sol')
+  const totalSupply = 1000
 
   it('should deploy the CLN token.', async () => {
-    let cln = await ColuLocalNetwork.new(totalSupply, {from: accounts[0]});
-    let balance = await cln.balanceOf(accounts[0]);
-    console.log('balance', balance.toString());
-    assert.equal(balance.valueOf(), totalSupply, totalSupply + " wasn't in the first account");
+    let cln = await ColuLocalNetwork.new(totalSupply, {from: accounts[0]})
+    let balance = await cln.balanceOf(accounts[0])
+    console.log('balance', balance.toString())
+    assert.equal(balance.valueOf(), totalSupply, totalSupply + " wasn't in the first account")
   })
-
-});
+})
