d68a6886807267ee09e3882e217878304e879465
Lior Rabin <lior@colu.com>
2018-08-19 15:54:04 +0300

    PR comments

--- test/community.js
+++ test/community.js
@@ -26,21 +26,22 @@ contract('COMMUNITY', async (accounts) => {
 
   const communityName = 'Test Community'
 
-  const validateCommunity = (community, currency, communityCustomName) => {
-    expect(community).to.be.a('Object')
-    expect(community.id).to.be.a('string')
-    expect(community.name).to.equal(communityCustomName || communityName)
-    expect(community.wallets).to.be.an('array')
-    expect(community.wallets).to.have.lengthOf(3)
-    expect(community.mnemonic).to.be.a('string')
-    expect(community.defaultCurrency.toString()).to.equal(currency.id)
-    community.wallets.forEach(wallet => {
+  const validateCommunity = (community1, currency, communityCustomName, community2) => {
+    expect(community1).to.be.a('Object')
+    expect(community1.id).to.be.a('string')
+    if (community2) expect(community1.id).to.equal(community2.id)
+    expect(community1.name).to.equal(community2 ? community2.name : (communityCustomName || communityName))
+    expect(community1.wallets).to.be.an('array')
+    expect(community1.wallets).to.have.lengthOf(3)
+    expect(community1.mnemonic).to.be.a('string')
+    expect(community1.defaultCurrency.toString()).to.equal(community2 ? community2.defaultCurrency.toString() : currency.id)
+    community1.wallets.forEach(wallet => {
       expect(['manager', 'users', 'merchants']).to.contain(wallet.type)
       expect(wallet.address).to.be.a('string')
       expect(wallet.index).to.be.a('number')
       expect(wallet.balances).to.be.an('array')
       expect(wallet.balances).to.have.lengthOf(1)
-      expect(wallet.balances[0].currency.toString()).to.equal(currency.id)
+      expect(wallet.balances[0].currency.toString()).to.equal(community2 ? community2.defaultCurrency.toString() : currency.id)
       expect(wallet.balances[0].blockchainBalance.toNumber()).to.equal(0)
       expect(wallet.balances[0].offchainBalance.toNumber()).to.equal(0)
     })
@@ -58,10 +59,8 @@ contract('COMMUNITY', async (accounts) => {
     const currencyFactory = await CurrencyFactory.new(mmLib.address, cln.address, {from: accounts[0]})
     const result = await currencyFactory.createCurrency('TestLocalCurrency', 'TLC', 18, CC_MAX_TOKENS, 'ipfs://hash', {from: accounts[0]})
     ccAddress = result.logs[0].args.token
-    const cc = await ColuLocalCurrency.at(ccAddress)
 
     mmAddress = await currencyFactory.getMarketMakerAddressFromToken(ccAddress)
-    const mm = await EllipseMarketMaker.at(mmAddress)
 
     await currencyFactory.openMarket(ccAddress)
 
@@ -93,52 +92,52 @@ contract('COMMUNITY', async (accounts) => {
 
   it('should get community (by id)', async () => {
     let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    let community = await osseus.lib.Community.create(communityName, currency.id)
-    community = await osseus.db_models.community.getById(community.id)
-    validateCommunity(community, currency)
+    let community1 = await osseus.lib.Community.create(communityName, currency.id)
+    let community2 = await osseus.db_models.community.getById(community1.id)
+    validateCommunity(community1, currency, undefined, community2)
   })
 
   it('should get community (by name)', async () => {
     let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    let community = await osseus.lib.Community.create(communityName, currency.id)
-    community = await osseus.db_models.community.getByName(communityName)
-    validateCommunity(community, currency)
+    let community1 = await osseus.lib.Community.create(communityName, currency.id)
+    let community2 = await osseus.db_models.community.getByName(communityName)
+    validateCommunity(community1, currency, undefined, community2)
   })
 
   it('should get community (by wallet address)', async () => {
     let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    let community = await osseus.lib.Community.create(communityName, currency.id)
-    community = await osseus.db_models.community.getByWalletAddress(community.wallets[0].address)
-    validateCommunity(community, currency)
+    let community1 = await osseus.lib.Community.create(communityName, currency.id)
+    let community2 = await osseus.db_models.community.getByWalletAddress(community1.wallets[0].address)
+    validateCommunity(community1, currency, undefined, community2)
 
-    community = await osseus.db_models.community.getByWalletAddress(community.wallets[1].address)
-    validateCommunity(community, currency)
+    let community3 = await osseus.db_models.community.getByWalletAddress(community1.wallets[1].address)
+    validateCommunity(community1, currency, undefined, community3)
 
-    community = await osseus.db_models.community.getByWalletAddress(community.wallets[2].address)
-    validateCommunity(community, currency)
+    let community4 = await osseus.db_models.community.getByWalletAddress(community1.wallets[2].address)
+    validateCommunity(community1, currency, undefined, community4)
   })
 
   it('should get error if community not found (by id)', async () => {
     let fakeId = '123abc'
     let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    let community = await osseus.lib.Community.create(communityName, currency.id)
-    community = await osseus.db_models.community.getById(fakeId).catch(err => {
+    let community1 = await osseus.lib.Community.create(communityName, currency.id)
+    let community2 = await osseus.db_models.community.getById(fakeId).catch(err => {
       expect(err).not.to.be.undefined
     })
-    expect(community).to.be.undefined
+    expect(community2).to.be.undefined
   })
 
   it('should get all communities', async () => {
     let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    await osseus.lib.Community.create(`${communityName} #1`, currency.id)
-    await osseus.lib.Community.create(`${communityName} #2`, currency.id)
-    await osseus.lib.Community.create(`${communityName} #3`, currency.id)
+    let community1 = await osseus.lib.Community.create(`${communityName} #1`, currency.id)
+    let community2 = await osseus.lib.Community.create(`${communityName} #2`, currency.id)
+    let community3 = await osseus.lib.Community.create(`${communityName} #3`, currency.id)
     let communities = await osseus.db_models.community.getAll()
     expect(communities).to.be.an('array')
     expect(communities).to.have.lengthOf(3)
-    communities.forEach((community, i) => {
-      validateCommunity(community, currency, `${communityName} #${i + 1}`)
-    })
+    validateCommunity(communities[0], currency, `${communityName} #1`, community1)
+    validateCommunity(communities[1], currency, `${communityName} #2`, community2)
+    validateCommunity(communities[2], currency, `${communityName} #3`, community3)
   })
 
   after(async function () {
--- test/currency.js
+++ test/currency.js
@@ -24,13 +24,14 @@ contract('CURRENCY', async (accounts) => {
   const ccABI = JSON.stringify([{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwnerCandidate","type":"address"}],"name":"requestOwnershipTransfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"tokenURI","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"transferAndCall","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"newOwnerCandidate","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_tokenAddress","type":"address"},{"name":"_amount","type":"uint256"}],"name":"transferAnyERC20Token","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"remaining","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_tokenURI","type":"string"}],"name":"setTokenURI","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"_name","type":"string"},{"name":"_symbol","type":"string"},{"name":"_decimals","type":"uint8"},{"name":"_totalSupply","type":"uint256"},{"name":"_tokenURI","type":"string"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[{"indexed":false,"name":"newTokenURI","type":"string"}],"name":"TokenURIChanged","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"by","type":"address"},{"indexed":true,"name":"to","type":"address"}],"name":"OwnershipRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"},{"indexed":false,"name":"data","type":"bytes"}],"name":"TransferAndCall","type":"event"}])
   const mmABI = JSON.stringify([{"constant":true,"inputs":[{"name":"token","type":"address"}],"name":"supportsToken","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_newOwnerCandidate","type":"address"}],"name":"requestOwnershipTransfer","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"operational","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token2","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"mmLib","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"S2","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"S1","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"acceptOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"PRECISION","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"R1","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_sender","type":"address"},{"name":"_value","type":"uint256"},{"name":"_data","type":"bytes"}],"name":"tokenFallback","outputs":[{"name":"ok","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"newOwnerCandidate","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"token1","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"openForPublic","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"R2","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"inputs":[{"name":"_mmLib","type":"address"},{"name":"_token1","type":"address"},{"name":"_token2","type":"address"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"payable":false,"stateMutability":"nonpayable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,"name":"by","type":"address"},{"indexed":true,"name":"to","type":"address"}],"name":"OwnershipRequested","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"}],"name":"OwnershipTransferred","type":"event"}])
 
-  const validateCurrency = (currency) => {
-    expect(currency).to.be.a('Object')
-    expect(currency.id).to.be.a('string')
-    expect(currency.ccAddress).to.equal(ccAddress)
-    expect(currency.mmAddress).to.equal(mmAddress)
-    expect(currency.ccABI).to.equal(ccABI)
-    expect(currency.mmABI).to.equal(mmABI)
+  const validateCurrency = (currency1, currency2) => {
+    expect(currency1).to.be.a('Object')
+    expect(currency1.id).to.be.a('string')
+    if (currency2) expect(currency1.id).to.equal(currency2.id)
+    expect(currency1.ccAddress).to.equal(currency2 ? currency2.ccAddress : ccAddress)
+    expect(currency1.mmAddress).to.equal(currency2 ? currency2.mmAddress : mmAddress)
+    expect(currency1.ccABI).to.equal(currency2 ? currency2.ccABI : ccABI)
+    expect(currency1.mmABI).to.equal(currency2 ? currency2.mmABI : mmABI)
   }
 
   before(async function () {
@@ -45,10 +46,8 @@ contract('CURRENCY', async (accounts) => {
     const currencyFactory = await CurrencyFactory.new(mmLib.address, cln.address, {from: accounts[0]})
     const result = await currencyFactory.createCurrency('TestLocalCurrency', 'TLC', 18, CC_MAX_TOKENS, 'ipfs://hash', {from: accounts[0]})
     ccAddress = result.logs[0].args.token
-    const cc = await ColuLocalCurrency.at(ccAddress)
 
     mmAddress = await currencyFactory.getMarketMakerAddressFromToken(ccAddress)
-    const mm = await EllipseMarketMaker.at(mmAddress)
 
     await currencyFactory.openMarket(ccAddress)
 
@@ -86,38 +85,38 @@ contract('CURRENCY', async (accounts) => {
   })
 
   it('should get currency (by id)', async () => {
-    let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    currency = await osseus.db_models.currency.getById(currency.id)
-    validateCurrency(currency)
+    let currency1 = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
+    let currency2 = await osseus.db_models.currency.getById(currency1.id)
+    validateCurrency(currency1, currency2)
   })
 
   it('should get currency (by cc address)', async () => {
-    let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    currency = await osseus.db_models.currency.getByCurrencyAddress(currency.ccAddress)
-    validateCurrency(currency)
+    let currency1 = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
+    let currency2 = await osseus.db_models.currency.getByCurrencyAddress(currency1.ccAddress)
+    validateCurrency(currency1, currency2)
   })
 
   it('should get currency (by mm address)', async () => {
-    let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    currency = await osseus.db_models.currency.getByMarketMakerAddress(currency.mmAddress)
-    validateCurrency(currency)
+    let currency1 = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
+    let currency2 = await osseus.db_models.currency.getByMarketMakerAddress(currency1.mmAddress)
+    validateCurrency(currency1, currency2)
   })
 
   it('should get error if currency not found (by id)', async () => {
     let fakeId = '123abc'
-    let currency = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
-    currency = await osseus.db_models.currency.getById(fakeId).catch(err => {
+    let currency1 = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
+    let currency2 = await osseus.db_models.currency.getById(fakeId).catch(err => {
       expect(err).not.to.be.undefined
     })
-    expect(currency).to.be.undefined
+    expect(currency2).to.be.undefined
   })
 
   it('should get all currencies', async () => {
-    await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
+    let currency1 = await osseus.lib.Currency.create(ccAddress, mmAddress, ccABI, mmABI)
     let currencies = await osseus.db_models.currency.getAll()
     expect(currencies).to.be.an('array')
     expect(currencies).to.have.lengthOf(1)
-    validateCurrency(currencies[0])
+    validateCurrency(currency1, currencies[0])
   })
 
   after(async function () {
