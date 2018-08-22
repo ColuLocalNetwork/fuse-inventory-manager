e28c80e9ad73ad8b0c5f13f8dcedfe826639e34e
Lior Rabin <lior@colu.com>
2018-08-22 17:02:43 +0300

    oops

--- modules/lib/index.js
+++ modules/lib/index.js
@@ -22,7 +22,8 @@ const init = (osseus) => {
   return new Promise(async (resolve, reject) => {
     this.osseus.lib = {
       Community: require('./models/community')(this.osseus),
-      Currency: require('./models/currency')(this.osseus)
+      Currency: require('./models/currency')(this.osseus),
+      Transaction: require('./models/transaction')(this.osseus)
     }
 
     this.osseus.communities = await getAllCommunities().catch(err => { return reject(err) })
