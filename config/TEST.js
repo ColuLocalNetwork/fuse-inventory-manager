b12375480613023f4dfcae9cc38d26764f559c04
Lior Rabin <lior@colu.com>
2018-08-26 14:00:23 +0300

    osseus-server@0.1.4

--- config/TEST.js
+++ config/TEST.js
@@ -5,6 +5,8 @@ module.exports = {
   OSSEUS_SERVER_DEPENDENCIES: ['logger', 'mongo'],
   OSSEUS_SERVER_PORT: '8080',
   OSSEUS_SERVER_MORGAN_FORMAT: ':date[iso] method=":method", url=":url", statusCode=":status", route=":route", host=":host", client-ip=":client-ip", user-agent=":user-agent", httpVersion=":http-version", responseTime=":response-time"',
+  OSSEUS_SERVER_ADD_HEALTHCHECK: true,
+  OSSEUS_SERVER_ADD_IS_RUNNING: true,
   OSSEUS_ROUTER_DEPENDENCIES: ['logger', 'server'],
   OSSEUS_ROUTER_ROUTES_PATH: '/app/routes',
   OSSEUS_ROUTER_CONTROLLERS_PATH: '/app/controllers',
