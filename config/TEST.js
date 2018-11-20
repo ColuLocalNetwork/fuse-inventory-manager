4a5d8dfe24e5b7c0f21df91bea9c3b03d226016c
jeremiG <gendronjeremi@gmail.com>
2018-11-20 02:28:16 -0500

    rm changes to test.js config

--- config/TEST.js
+++ config/TEST.js
@@ -14,7 +14,7 @@ module.exports = {
   OSSEUS_ROUTER_URL_PREFIX: '/api',
   OSSEUS_ROUTER_JWT_SECRET: '3g4Zy5rM7p9nV7Y3Xl7zKY4IvJo2n5lFUUTwHUGPn6',
   OSSEUS_MONGO_DEPENDENCIES: ['logger'],
-  OSSEUS_MONGO_URI: 'mongodb://mongo/inventory-manager-tests',
+  OSSEUS_MONGO_URI: 'mongodb://localhost/inventory-manager-tests',
   WEB3_PROVIDER: 'http://127.0.0.1:7545',
   SECRET: '46fffed0-d51f-4c54-9096-e9e95e3643fbthisissomekindofsecret',
   BLOCKS_TO_FINALIZE_BCTX: 20
